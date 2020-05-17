524b4291f060370bd969be30a4688ab7e7a5974c
julio olea <julio.olea@skipthedishes.ca>
2020-05-17 10:10:32 -0600

    Add new tests service to compose

--- services/jenkins/init.sh
+++ services/jenkins/init.sh
@@ -4,7 +4,10 @@ chown jenkins:jenkins /var/run/docker.sock
 
 chown -R jenkins:jenkins /var/jenkins_home
 
-su -c apt update && apt install maven -y
+su -c sudo apt update && sudo apt -y install maven make
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 /usr/local/bin/plugins.sh /usr/jenkins/plugins.txt
 
