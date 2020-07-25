7f2790555359c6d3b9b3fa33a5bcead0c80378f4
julio olea <julio.olea@skipthedishes.ca>
2020-07-25 14:38:57 -0600

    Add jekins bind vol

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -11,6 +11,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from selenium.webdriver.chrome.options import Options
+# use env for servers
+# eg os.environ["SELENIUM"]
 
 class Test():
   def setup_method(self, method):
